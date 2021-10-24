9c2a043356aba2376a17a4124577e12a52b7bb24
ketakideuskar <46800284+ketakideuskar@users.noreply.github.com>
2021-10-23 19:49:56 -0700

    added files for backend

---
+++ getRecipes.py
@@ -0,0 +1,48 @@
+import json
+'''
+Post:
+    param: list of ingredients
+    [ing1, ing2, ing3]
+    ing1 --> set of ids
+    ing2 --> set of ids
+
+    resultSet = get first ingredient from map and get it's set
+    for each remaining ingredient k
+        get k's set of recipe ids -- set2
+        resultSet = resultSet intersect set2
+
+
+    with result set
+        pull from map and return it
+
+    set1, set2, set3, set4
+    set1 intersect set2 --> new set1
+    net set1 intersect set 3
+
+'''
+
+def getRecipes(ings):
+    ingToIdFile = open('ingToId.txt', mode ='r')
+    idToRecipeFile = open('idToRecipe.txt', mode = 'r')
+    ingToId = json.load(ingToIdFile)
+    idToRecipe = json.load(idToRecipeFile)
+    
+    resultSet = ingToId[ings[0]]
+    for ing in ings[1:]:
+        newSet = ingToId[ing]
+        resultSet = set(resultSet).intersection(newSet)
+    
+    recipes = []
+    print("result set: ", len(resultSet))
+    for id in resultSet:
+        recipe = idToRecipe[id]
+        recipes.append(recipe)
+    
+    return recipes
+
+
+def main():
+    print(getRecipes(["asparagus", "onion", "salmon"]))
+
+if __name__ == "__main__":
+    main()
---
+++ preprocessing.py
@@ -0,0 +1,83 @@
+import csv
+import json
+'''
+with open('RAW_recipes.csv', mode='r') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    count = 0;
+    test_file = open('test.txt', mode='w')
+    for row in csv_reader:
+        if (count < 5):
+            test_file.write(" ".join(row))
+            test_file.write('\n')
+            count += 1
+        else:
+            break
+'''
+
+ingToId = {}
+idToRecipe = {}
+
+with open('RAW_recipes.csv', mode='r') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    count = 0;
+    for row in csv_reader:
+        if (count == 0):
+            count += 1
+            continue
+
+        ings = row[10][2: (len(row[10]) - 2)].split("\', \'")
+        #temp = temp.replace("\'", "\"")
+        #ings = json.loads(temp)
+        
+        steps = row[8][2: (len(row[8]) - 2)].split("\', \'")
+        idToRecipe[row[1]] = {'Name': row[0], 'Minutes': row[2], 'Steps': steps, 'Description': row[9], 'Ingredients': ings}
+        
+        for key in ings:
+            if key not in ingToId.keys():
+                ingToId[key] = []
+            ingToId[key].append(row[1])
+        count += 1
+        print(count)
+
+    with open('ingToId.txt', mode='w') as ingDump:
+        ingDump.write(json.dumps(ingToId))
+    with open('idToRecipe.txt', mode='w') as idDump:
+        idDump.write(json.dumps(idToRecipe))
+    print("done")
+    
+    count2 = 0
+    with open('ingToId.txt', mode='r') as ingDump2:
+        data = json.load(ingDump2)
+        for key in data:
+            if (count2 < 5):
+                print(key)
+                count2 += 1
+            else:
+                break
+    count2 = 0             
+    with open('idToRecipe.txt', mode='r') as idDump2:
+        data2 = json.load(idDump2)
+        for key in data2:
+            if (count2 < 5):
+                print(key)
+                count2 += 1
+            else:
+                break
+
+    
+
+'''
+Post:
+    param: list of ingredients
+    for each ingredient k
+        get k's set of recipe ids
+    for each set of recipe ids
+        intersect with the next id
+    with result set
+        pull from map and return it
+
+    set1, set2, set3, set4
+    set1 intersect set2 --> new set1
+    net set1 intersect set 3
+
+'''
